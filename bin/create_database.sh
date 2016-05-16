614c944919fe6602947604881f65a158bfb68feb
simonsdave <simonsdave@gmail.com>
2016-05-15 21:40:42 -0500

    better create_database.sh logging & error checks

--- bin/create_database.sh
+++ bin/create_database.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#
+# this script automates the process of building a postgress docker
+# image that runs postgres and contains a fully populated database
+# of current vulnerabilities.
 
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
@@ -34,10 +38,24 @@ DOCKERHUB_EMAIL=${2:-}
 DOCKERHUB_PASSWORD=${3:-}
 
 CLAIR_DATABASE_IMAGE_NAME=$DOCKERHUB_USERNAME/clair-database:$TAG
+CLAIR_IMAGE_NAME=quay.io/coreos/clair:latest
+CLAIR_CONTAINER_NAME=clair
 
-docker pull postgres:9.5.2
+DATABASE_SERVER_DOCKER_IMAGE=postgres:9.5.2
+
+echo "pulling database server docker image $DATABASE_SERVER_DOCKER_IMAGE"
+
+docker pull $DATABASE_SERVER_DOCKER_IMAGE >& /dev/null
+if [ $? != 0 ]; then
+    echo "error pulling database server docker image $DATABASE_SERVER_DOCKER_IMAGE" >&2
+    exit 1
+fi
+
+echo "successfully pulled database server docker image"
 
 CLAIR_DATABASE_CONTAINER_NAME=clair-database
+
+echo "starting database server"
 
 docker kill $CLAIR_DATABASE_CONTAINER_NAME >& /dev/null
 docker rm $CLAIR_DATABASE_CONTAINER_NAME >& /dev/null
@@ -46,30 +64,57 @@ docker \
     run \
     --name $CLAIR_DATABASE_CONTAINER_NAME \
     -e 'PGDATA=/var/lib/postgresql/data-non-volume' \
-    -p 9000:5432 \
     -d \
-    postgres:9.5.2
+    $DATABASE_SERVER_DOCKER_IMAGE \
+    > /dev/null
 
-echo -n 'Waiting for postgres to start '
+echo -n "waiting for database server to start "
 for i in $(seq 1 10)
 do
-    docker exec $CLAIR_DATABASE_CONTAINER_NAME sh -c 'echo "\list" | psql -U postgres' >& /dev/null
+    docker \
+        exec \
+        $CLAIR_DATABASE_CONTAINER_NAME \
+        sh -c 'echo "\list" | psql -U postgres' \
+        >& /dev/null
     if [ $? == 0 ]; then
         break
     fi
-    echo -n '.'
+    echo -n "."
     sleep 3
 done
-echo '.'
+echo ''
+
+echo "successfully started database server"
+
+echo "creating database"
 
 docker \
     exec \
     $CLAIR_DATABASE_CONTAINER_NAME \
-    sh -c 'echo "create database clair" | psql -U postgres'
+    sh -c 'echo "create database clair" | psql -U postgres' \
+    > /dev/null
 
+docker \
+    exec \
+    $CLAIR_DATABASE_CONTAINER_NAME \
+    sh -c 'echo "\list" | psql -U postgres' | \
+    grep '^\s*clair' \
+    > /dev/null
+if [ $? != 0 ]; then
+    echo "error creating database" >&2
+    exit 1
+fi
+
+#
 # create clair configuration that will point clair @ the database we just created
+#
+
+echo "successfully created database"
+
 CLAIR_CONFIG_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
 CLAIR_CONFIG_YAML=$CLAIR_CONFIG_DIR/config.yaml
+
+echo "creating clair configuration @ '$CLAIR_CONFIG_YAML'"
 
 curl \
     -s \
@@ -81,17 +126,18 @@ curl \
 # http://www.postgresql.org/docs/9.5/static/libpq-connect.html#LIBPQ-CONNSTRING
 sed \
     -i \
-    -e 's|source:|source: postgresql://postgres@clair-database:9000/clair?sslmode=disable|g' \
+    -e 's|source:|source: postgresql://postgres@clair-database:5432/clair?sslmode=disable|g' \
     "$CLAIR_CONFIG_YAML"
+
+echo "created clair configuration"
 
 docker \
     pull \
-    quay.io/coreos/clair:latest
+    $CLAIR_IMAGE_NAME \
+    > /dev/null
 
-CLAIR_CONTAINER_NAME=clair
-
-docker kill $CLAIR_CONTAINER_NAME >& /dev/null || true
-docker rm $CLAIR_CONTAINER_NAME >& /dev/null || true
+docker kill $CLAIR_CONTAINER_NAME >& /dev/null
+docker rm $CLAIR_CONTAINER_NAME >& /dev/null
 
 docker \
     run \
@@ -101,23 +147,24 @@ docker \
     --link $CLAIR_DATABASE_CONTAINER_NAME:clair-database \
     -v /tmp:/tmp \
     -v $CLAIR_CONFIG_DIR:/config \
-    quay.io/coreos/clair:latest \
-    -config=/config/config.yaml
+    $CLAIR_IMAGE_NAME \
+    -config=/config/config.yaml \
+    > /dev/null
 
-echo -n 'Waiting for vulnerabilities database update to finish '
+echo -n "Waiting for vulnerabilities database update to finish "
 while true
 do
     docker logs $CLAIR_CONTAINER_NAME | grep "updater: update finished" >& /dev/null
     if [ $? == 0 ]; then
         break
     fi
-    echo -n '.'
+    echo -n "."
     sleep 15
 done
-echo ''
+echo ""
 
-docker kill $CLAIR_CONTAINER_NAME
-docker rm $CLAIR_CONTAINER_NAME
+docker kill $CLAIR_CONTAINER_NAME > /dev/null
+docker rm $CLAIR_CONTAINER_NAME > /dev/null
 
 docker rmi $CLAIR_DATABASE_IMAGE_NAME >& /dev/null
 
