8e218f350b34c2cd4d808a1d4ed92a006a139548
Dave Simons <simonsdave@gmail.com>
2018-10-27 10:46:47 -0400

    fix silly error in 73a2c450b2534afbe6d0b

--- dev_env/create_dev_env.sh
+++ dev_env/create_dev_env.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
 if [ $# != 4 ]; then
     echo "usage: $(basename "$0") <github username> <github email> <github public key> <github private key>" >&2
     exit 1
