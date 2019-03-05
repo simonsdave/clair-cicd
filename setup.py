e76996e06c01fa1cc031b0001896e9144ba953dd
Dave Simons <simonsdave@gmail.com>
2019-03-04 21:41:43 -0500

    jsonschema 3.0.0 -> 3.0.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'bin/assess-vulnerabilities-risk.py',
     ],
     install_requires=[
-        "jsonschema==3.0.0",
+        'jsonschema==3.0.1',
     ],
     dependency_links=[
     ],
