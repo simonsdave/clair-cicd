43e28947687e9e5b0162868dd29ee561347cc650
Dave Simons <simonsdave@gmail.com>
2020-01-05 16:07:09 -0500

    test: add unit tests for clair_cicd.__init__.py

---
+++ clair_cicd/tests/__init__.py
@@ -0,0 +1,14 @@
+import unittest
+
+from .. import __version__
+from .. import __clair_version__
+
+class SomethingTestCase(unittest.TestCase):
+
+    def test_version(self):
+        self.assertIsNotNone(__version__)
+        self.assertTrue(0 < len(__version__))
+
+    def test_clair_version(self):
+        self.assertIsNotNone(__clair_version__)
+        self.assertTrue(0 < len(__clair_version__))
