9ac6de3905aaa9dca65dbc5811bbb5363390c259
Dave Simons <simonsdave@gmail.com>
2020-01-14 20:12:28 -0500

    fix: cut-release.sh repo specific extenstions

--- .cut-release-master-branch-changes.sh
+++ .cut-release-master-branch-changes.sh
@@ -11,15 +11,15 @@ if [ $# != 0 ]; then
     exit 1
 fi
 
-INIT_DOT_PY=$(repo-root-dir.sh)/$(repo.sh -u)/__init__.py
-CURRENT_VERSION=$(grep __version__ "$INIT_DOT_PY" | sed -e "s|^.*=[[:space:]]*['\"]||g" | sed -e "s|['\"].*$||g")
-
 # the pip install below is necessary make it super simple
 # to figure out the project's next version
-pip3 install semantic-version > /dev/null
+pip install semantic-version > /dev/null
 
-NEXT_VERSION=$(python -c "import semantic_version; print(semantic_version.Version('$CURRENT_VERSION').next_minor())")
+CURRENT_VERSION=$(python-version.sh)
 
+NEXT_VERSION=$(python -c "import semantic_version; print(semantic_version.Version('$CURRENT_VERSION').next_patch())")
+
+INIT_DOT_PY=$(repo-root-dir.sh)/$(repo.sh -u)/__init__.py
 sed -i '' -e "s|^[[:space:]]*__version__[[:space:]]*=[[:space:]]*['\"]${CURRENT_VERSION}['\"][[:space:]]*$|__version__ = '${NEXT_VERSION}'|g" "${INIT_DOT_PY}"
 
 exit 0
--- .cut-release-release-branch-changes.sh
+++ .cut-release-release-branch-changes.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# :TODO: more work is require here ...
+# <release-branch> is assumed to be something like "release-0.9.32"
 
 set -e
-
-SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
 if [ $# != 1 ]; then
     echo "usage: $(basename "$0") <release-branch>" >&2
@@ -13,15 +11,50 @@ fi
 
 RELEASE_BRANCH=${1:-}
 
+REPO_ROOT_DIR=$(repo-root-dir.sh)
+
+#
+# badges
+#
+
+# requires.io
+sed -i '' \
+    -e \
+    "s|?branch=master|?branch=${RELEASE_BRANCH}|g" \
+    "${REPO_ROOT_DIR}/README.md"
+
+# CircleCI
 sed -i '' \
     -e \
     "s|/tree/master|/tree/${RELEASE_BRANCH}|g" \
-    "${SCRIPT_DIR_NAME}/README.md"
+    "${REPO_ROOT_DIR}/README.md"
 
-rm -f "${SCRIPT_DIR_NAME}/README.rst"
+# codecov
+sed -i '' \
+    -e \
+    "s|/branch/master|/branch/${RELEASE_BRANCH}|g" \
+    "${REPO_ROOT_DIR}/README.md"
+
+# don't need to do anything for docker images
+
+#
+# references to files in docs and bin directories of repo
+#
+
+sed -i '' \
+    -e \
+    "s|(docs|(https://github.com/simonsdave/clair-cicd/blob/${RELEASE_BRANCH}/bin|g" \
+    "${REPO_ROOT_DIR}/README.md"
+
+sed -i '' \
+    -e \
+    "s|(bin|(https://github.com/simonsdave/clair-cicd/blob/${RELEASE_BRANCH}/bin|g" \
+    "${REPO_ROOT_DIR}/README.md"
+
+rm -f "${REPO_ROOT_DIR}/README.rst"
 build-readme-dot-rst.sh
 
-rm -rf "${SCRIPT_DIR_NAME}/dist"
+rm -rf "${REPO_ROOT_DIR}/dist"
 build-python-package.sh
 
 exit 0
