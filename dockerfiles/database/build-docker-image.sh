d474153c16abc3abafe9b4a108ce3eb2596c2b57
Dave Simons <simonsdave@gmail.com>
2020-04-08 18:27:16 -0400

    fix: rm dockerfiles/database/build-docker-image.sh's dependance on dev-env-version.txt

--- dockerfiles/database/build-docker-image.sh
+++ dockerfiles/database/build-docker-image.sh
@@ -15,10 +15,6 @@ DOCKER_CONTAINER_NAME=$(openssl rand -hex 16)
 
 DUMMY_DOCKER_CONTAINER_NAME=$(create-dummy-docker-container.sh)
 
-IMAGE_NAME_TEMPLATE=$(grep FROM "${REPO_ROOT_DIR}/dev_env/Dockerfile.template" | sed -e "s|^FROM[[:space:]]*||g")
-DEV_ENV_VERSION=$(cat "${REPO_ROOT_DIR}/dev_env/dev-env-version.txt")
-IMAGE_NAME=${IMAGE_NAME_TEMPLATE//%DEV_ENV_VERSION%/${DEV_ENV_VERSION}}
-
 IN_CONTAINER_SCRIPT_DIR_NAME=${SCRIPT_DIR_NAME//${REPO_ROOT_DIR}//app}
 
 #
@@ -31,7 +27,7 @@ docker run \
     --name "${DOCKER_CONTAINER_NAME}" \
     --volumes-from "${DUMMY_DOCKER_CONTAINER_NAME}" \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    "${IMAGE_NAME}" \
+    "$(get-circle-ci-executor.sh)" \
     /bin/bash -c "${IN_CONTAINER_SCRIPT_DIR_NAME}/_build-docker-image.sh ${CLAIR_DATABASE_IMAGE_NAME}"
 EXIT_CODE=$?
 
